1626c613ea1efdcc3c9910014a7b53a127a2dfbd
Deon Odendaal <deon.odendaal@protonmail.com>
2019-09-11 16:19:55 +0100

    redux dev tools & redux forms

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -25,7 +25,7 @@ class GoogleAuth extends React.Component {
   // HELPER METHODS
   onAuthChange = isSignedIn => {
     if (isSignedIn) {
-      this.props.signIn(this.this.auth.currentuser.get().getId());
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
+// Redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // create redux store
-const store = createStore(reducers);
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
