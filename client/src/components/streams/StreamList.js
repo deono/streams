c83f49a8061fc0e181c9dce0476dfd5e6f6c3b35
Deon Odendaal <deon.odendaal@protonmail.com>
2019-09-12 13:14:33 +0100

    SteamList

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,75 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          {this.renderAdminButtons(stream)}
+          <i className="large middle aligned icon camera"></i>
+          <div className="content">
+            <div className="header">{stream.title}</div>
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  renderAdminButtons = stream => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content ui icon buttons">
+          <button className="mini ui primary button">
+            <i className="edit icon"></i>
+          </button>
+          <button className="mini ui negative button">
+            <i className="delete icon"></i>
+          </button>
+        </div>
+      );
+    }
+  };
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
-export default StreamList;
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);
