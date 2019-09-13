91ad6afb58562fa84c2a29cc7c1b4b159c4073ea
Deon Odendaal <deon.odendaal@protonmail.com>
2019-09-13 10:23:17 +0100

    StreamEdit component

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,16 +1,31 @@
+import _ from "lodash";
 import React from "react";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
 import { connect } from "react-redux";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div className="ui active inline loader"></div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(this.props.stream, "title", "description")}
+        />
+      </div>
+    );
   }
 }
 
@@ -22,5 +37,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
